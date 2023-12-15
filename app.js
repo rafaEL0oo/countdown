034121544b21b9089fbcb47c8ba1889a0ba40cba
rafaEL0oo <88623317+rafaEL0oo@users.noreply.github.com>
2023-12-15 12:34:30 +0100

    new countdown

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 17, 2023 21:37:00").getTime();
+var countDownDate = new Date("Jun 30, 2024 10:00:00").getTime();
+const DAYS_COUNT = document.querySelector('.days h2')
+
+const HOURS_COUNT = document.querySelector('.hours h2')
+
+const MINUTES_COUNT = document.querySelector('.minutes h2')
+
+const SECONDS_COUNT = document.querySelector('.seconds h2')
+
+
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -17,12 +26,16 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  // document.getElementById("demo").innerHTML = `<b>${days}</b>d <b>${hours}</b>h <b>${minutes}</b>m <b>${seconds}</b>s`;
+
+  DAYS_COUNT.textContent = days
+  HOURS_COUNT.textContent = hours
+  MINUTES_COUNT.textContent = minutes
+  SECONDS_COUNT.textContent = seconds
     
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "MOŻNA PIĆ!!!";
+    document.getElementById("demo").innerHTML = '<img src="/img/the-end.png" alt="the end">'
   }
 }, 1000);
